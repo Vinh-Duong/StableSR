9987c5d461eac7b625ee22b7e0d33f69aff2aef8
Vinh Duong <44298848+Vinh-Duong@users.noreply.github.com>
2024-08-26 21:51:26 +0900

    add generate data

--- scripts/train/generate_data.sh
+++ scripts/train/generate_data.sh
@@ -3,14 +3,12 @@
 # General SR
 python scripts/generate_vqgan_data.py \
 --config configs/stableSRdata/test_data.yaml \
---ckpt D:/Vinh/3.Project_working/git_tutorial/StableSR/checkpoints/stablesr_turbo.ckpt \
+--ckpt D:/Vinh/3.Project_working/git_tutorial/StableSR/checkpoints/stablesr_000117.ckpt \
 --outdir D:/Vinh/3.Project_working/diffusion/StableSR/output/generate_vqgan_train_data \
 --skip_grid \
---ddpm_steps 4 \
+--ddpm_steps 200 \
 --base_i 0 \
 --seed 10000
-
-
 
 # For face data
 # python scripts/generate_vqgan_data_face.py \
